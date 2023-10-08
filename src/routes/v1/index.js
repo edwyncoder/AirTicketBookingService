3859d553552d8a4fbfc398a8aaba5597856b0305
edwyncoder <edwyn.t@flyerssoft.com>
2023-10-08 10:50:47 +0530

    added messageQueue config setup

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,6 +8,6 @@ const bookingController = new BookingController();
 const router = express.Router();
 
 router.post('/bookings', bookingController.create);
-// router.post('/publish', bookingController.sendMessageToQueue);
+router.post('/publish', bookingController.sendMessageToQueue);
 
 module.exports = router;
